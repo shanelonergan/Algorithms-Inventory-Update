228b9be4ce408a875167a0c6703fdb7567c8d23b
shanelonergan <sptlonergan@gmail.com>
2019-12-13 15:56:12 -0500

    1 file modified

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-console.log('test')
+// algo
+
+function updateInventory(arr1, arr2) {
+    // All inventory must be accounted for or you're fired!
+    const currentInvNames = currentInv.map(item => item[1])
+    console.log(currentInvNames)
+}
+
+// Example inventory lists
+var currentInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+console.log(updateInventory(currentInv, newInv));
