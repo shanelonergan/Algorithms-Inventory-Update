3a589f6fd4e39565acdf7bed05759296b893ad9c
shanelonergan <sptlonergan@gmail.com>
2019-12-13 16:34:02 -0500

    1 file modified

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 function updateInventory(currentArr, newArr) {
     // All inventory must be accounted for or you're fired!
-    function findItem(name) {
-      const item = currentArr.filter(item => item[1] === name)
-      console.log(14, item)
-
-      return currentArr.indexOf(item)
+    function updateItem(name, newItem) {
+      updatedArr = currentArr.map(item => {
+        if (item[1] === name) {
+         
+          item[0] += newItem[0]
+        }
+        })
     }
 
     const currentInvNames = currentArr.map(item => item[1])
     console.log(currentInvNames)
 
-    newArr.forEach(item => {
-      const currentItemIndex = findItem(item[1])
+    newArr.forEach(newItem => {
 
-      console.log(currentItemIndex)
 
-      if (currentInvNames.includes(item[1])) {
-        currentArr[0] = currentArr[0] + item[0] 
+      if (currentInvNames.includes(newItem[1])) {
+        console.log(updateItem(newItem[1], newItem), 27)
       }
     })
 
